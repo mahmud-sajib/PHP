a45ebddbaba984ce6f35e76ed4afa09ddb39c93f
mahmud-sajib <32324883+mahmud-sajib@users.noreply.github.com>
2017-10-13 15:52:06 +0600

    Constant

--- class_constant.php
+++ class_constant.php
@@ -6,9 +6,7 @@
 
 2. Constant is by default public and they don't need access modifiers.
 
-2. Unlike properties constants once declared can never be changed
-
-3. The Constant can be accessed through this syntax:- ClassName::CONSTANT NAME  
+3. Unlike properties constants once declared can never be changed
 
 4. To access a constant we use this procedure :- ClassName::ConstantName
 
