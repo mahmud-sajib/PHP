8113b449b665db025e4f6e52e7fa2973a699da89
mahmud-sajib <32324883+mahmud-sajib@users.noreply.github.com>
2017-10-15 09:05:27 +0600

    Autoload Class

--- autoload_class.php
+++ autoload_class.php
@@ -1,15 +1,9 @@
 <?php
 
-//include "java.php";
-//include "ruby.php";
-
 // spl_autoload_register() loads all the classes from different files in a particular file.
 
 spl_autoload_register(function($class_name){
 	include "$class_name".".php";
 });
-
-$java = new Java;
-$ruby = new Ruby;
 
 ?>
