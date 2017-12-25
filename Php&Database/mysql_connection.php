994019e88d5c0efed2606d8444033887da8519ff
mahmud-sajib <32324883+mahmud-sajib@users.noreply.github.com>
2017-12-25 20:38:04 +0600

    Php and Database

---
+++ Php&Database/mysql_connection.php
@@ -0,0 +1,16 @@
+<?php
+
+// creating an object of mysqli class and passing parameters to it's constructor for database connection
+
+$db = new mysqli("localhost","niha","123456","hellodb");
+
+// mysqli_connect_errno() function returns the error code from last connection call
+
+if(mysqli_connect_errno()){
+	echo "Connection failed";
+	exit();
+} else {
+	echo "Connection successful";
+}
+
+?>
---
+++ Php&Database/mysqli_prepared_statement.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+  1. prepare() function prepares a query
+  2. execute() function executes a prepared query
+  3. bind_result() function binds variables to a prepared statemnet for result storage
+  4. fetch() function fetches results from a prepared statement into the bound variables
+*/
+
+$db = new mysqli("localhost","niha","123456","hellodb");
+
+if(mysqli_connect_errno()){
+	echo "Connection failed";
+	exit();
+} else {
+	//echo "Connection successful";
+}
+
+$sql = " select name, skill from students_info order by id ";
+
+// prepares a query
+$stmt = $db->prepare($sql);
+
+// executes a prepared query
+$stmt->execute();
+
+// binds variables to a prepared statemnet for result storage
+$stmt->bind_result($name, $skill);
+
+// fetch results from a prepared statement into the bound variables
+while($stmt->fetch()){
+	echo $name . " : " . $skill . "</br>";
+}
+
+?>
