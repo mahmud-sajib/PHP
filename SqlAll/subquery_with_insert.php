205f2da10a12d45624b2adbce7861ef11c63ac0a
mahmud-sajib <32324883+mahmud-sajib@users.noreply.github.com>
2018-01-16 20:24:31 +0600

    Update subquery_with_insert.php

--- SqlAll/subquery_with_insert.php
+++ SqlAll/subquery_with_insert.php
@@ -2,6 +2,6 @@
 
 // performing a subquery within SELECT statement
 
-$sqlQuery = "INSERT INTO customers_backup SELECT * FROM customers WHERE id IN(SELECT id FROM customers)";
+INSERT INTO customers_backup SELECT * FROM customers WHERE id IN(SELECT id FROM customers);
 
 ?>
